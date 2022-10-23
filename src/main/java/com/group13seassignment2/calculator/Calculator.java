36bed7c15af46e84a00e8819fc9989d424a08429
Harold Karibiye <haroldkaribiye@gmail.com>
2022-10-23 23:21:21 +0100

    Implement eval

--- src/main/java/com/group13seassignment2/calculator/Calculator.java
+++ src/main/java/com/group13seassignment2/calculator/Calculator.java
@@ -1,5 +1,8 @@
 package com.group13seassignment2.calculator;
 
+import java.util.Stack;
+
+
 public class Calculator {
     // VERY IMPORTANT: DO NOT ADD A MAIN() ANYWHERE
     // the entire thing will stop working if you add a main() method in any file
@@ -33,9 +36,71 @@ public class Calculator {
     // and then set the result to the answer to the equation (or an error message if input string was invalid)
     public void calculate(String rawInput){
         String correctInput = validateInput(rawInput);
+        String result = Double.toString(eval(correctInput));
 
         // IMPORTANT: the result has to be set at the end of the function, otherwise nothing will show up on the webpage
-        setResult(correctInput + " echoed");
+        setResult(result + " echoed");
+    }
+
+    private double eval(String correctInput) {
+        String[] tokens = getTokens(correctInput);
+        Stack<Double> nums = new Stack<Double>();
+        Stack<String> ops = new Stack<String>();
+
+        // each token is either a string representing an integer/double, an operator, parentheses
+        for(String token : tokens) {
+            if (isNumber(token)) {
+                nums.push(Double.parseDouble(token));
+            }
+            else if (token.equals("(")) {
+                ops.push(token);
+            }
+            else if (token.equals(")")) {
+                while (!ops.isEmpty() && !ops.peek().equals("(")) {
+                    evaluateStack(nums, ops);
+                }
+                ops.pop();   // pop the left parentheses
+            }
+            else {
+                // token must be an operator given that the input is valid
+                while (!ops.isEmpty() && getPrecedence(ops.peek()) >= getPrecedence(token)) {
+                    evaluateStack(nums, ops);
+                }
+                ops.push(token);
+            }
+        }
+
+        while (!ops.isEmpty()) {
+            evaluateStack(nums, ops);
+        }
+
+        return nums.pop();
+    }
+
+    private void evaluateStack(Stack<Double> nums, Stack<String> ops) {
+        double right = nums.pop();
+        double left = nums.pop();
+        String op = ops.pop();
+        nums.push(applyOp(left, right, op));
+    }
+
+    private int getPrecedence(String op) {
+        return 0;
+    }
+
+    private Double applyOp(double left, double right, String op) {
+        return 0.0;
+    }
+
+    private String[] getTokens(String correctInput) {
+        // replace exp(xxx) with the evaluation and parse parentheses, operators and doubles/Integers
+        String[] tokens = new String[] {""};
+
+        return tokens;
+    }
+
+    private boolean isNumber(String value) {
+        return false;
     }
 
     // getter and setter functions, please do not change these
