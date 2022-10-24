49b46a6b81ac0304e8c1b8404b0cee165d51237b
francsir <72939196+francsir@users.noreply.github.com>
2022-10-24 13:26:02 +0100

    removed printstatement

--- src/test/java/com/group13seassignment2/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/group13seassignment2/calculator/CalculatorApplicationTests.java
@@ -14,14 +14,15 @@ class CalculatorApplicationTests {
     // Please do not change anything above this comment (other than adding imports), it's essential
     // for the whole thing to work properly!
 
+    //Tests for calculate without stringValidation.
+ 
     @Test
     void testCalculate(){
         Calculator calc = new Calculator();
-        String [][] testValues = {{"6/2*(1+2)","6/(2*(1+2))","3+5*exp(4.2)/(5+7)"},{"9.0","1.0","30.786"}};
+        String [][] testValues = {{"6/2*(1+2)","6/(2*(1+2))","exp(4)","3+5*exp(4.2)/(5+7)"},{"9.0","1.0","54.598","30.786"}};
         for(int i = 0; i < testValues[0].length; i++)
         {
             String result = unitTestCalc(testValues[0][i], calc);
-            System.out.println(result + "  " + testValues[1][i]);
             assertEquals(result, testValues[1][i]);
         }
         
