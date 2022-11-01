402a289b355aa834f6262427a78f53b73bc95ff7
goleccc <113783833+golecl@users.noreply.github.com>
2022-11-01 17:19:16 +0000

    updated comments in calculator

--- src/main/java/com/group13seassignment2/calculator/Calculator.java
+++ src/main/java/com/group13seassignment2/calculator/Calculator.java
@@ -7,21 +7,16 @@ import java.util.Stack;
 
 
 public class Calculator {
-    // VERY IMPORTANT: DO NOT ADD A MAIN() ANYWHERE
-    // the entire thing will stop working if you add a main() method in any file
     private String input;
     private String result;
 
     public Calculator(){
-        // please don't change this constructor, it is used for the GUI
+        // Constructor used for the GUI
         setInput("");
         setResult("");
     }
     // validateInput function
     // Parameters: String rawInput
-    // Return: String correctInput
-    // CURRENTLY: just returns the raw input
-    // IN THE FUTURE: Takes in raw input, checks if the input is valid
     // If input is valid then it returns it in whatever format is needed for the calculator functions
     // If input is invalid, it returns an error message
     public String validateInput(String rawInput) throws Exception{
@@ -277,9 +272,6 @@ public class Calculator {
     // Parameters: String rawInput
     // Return: nothing
     // Takes in the raw input, calls the validateInput function
-    // CURRENTLY: just sets the result to the raw input with " echoed" added to it
-    // IN THE FUTURE: will call the validateInput function, and then run all the calculation functions
-    // and then set the result to the answer to the equation (or an error message if input string was invalid)
     public void calculate(String rawInput){
         String result = "";
         try {
@@ -420,8 +412,7 @@ public class Calculator {
     }
 
 
-    // getter and setter functions, please do not change these
-    // also please do not set or get the variables "input" and "string" in any other way
+    // getter and setter functions
 
     public String getInput(){
         return input;
